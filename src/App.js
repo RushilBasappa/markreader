91d82cbfc4d04e005aad09bf9fd972965452c38c
Rushil Basappa <rushil.oucse37@gmail.com>
2020-11-25 04:35:50 +0530

    add split panes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SplitPane, { Pane } from "react-split-pane";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitPane split="vertical" defaultSize="50%" minSize={100}>
+      <Pane initialSize="50%">Left</Pane>
+      <Pane initialSize="50%">Right</Pane>
+    </SplitPane>
   );
 }
 
