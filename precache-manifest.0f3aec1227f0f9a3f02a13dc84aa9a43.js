9ac97b7df7cd09e621d0026638625da8d30e9bfe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-25 02:34:25 +0000

    Deploy RushilBasappa/markreader to github.com/RushilBasappa/markreader.git:gh-pages

---
+++ precache-manifest.0f3aec1227f0f9a3f02a13dc84aa9a43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285a0bdb27222ae5ce8fdb97c5210286",
+    "url": "/markreader/index.html"
+  },
+  {
+    "revision": "2209efbd007aff179705",
+    "url": "/markreader/static/css/2.b2f02b1f.chunk.css"
+  },
+  {
+    "revision": "5188fc7cd129c2ad1212",
+    "url": "/markreader/static/css/main.a6f0b736.chunk.css"
+  },
+  {
+    "revision": "2209efbd007aff179705",
+    "url": "/markreader/static/js/2.09bc29ce.chunk.js"
+  },
+  {
+    "revision": "b77a62c2f0dce35513c08e3872fecda7",
+    "url": "/markreader/static/js/2.09bc29ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5188fc7cd129c2ad1212",
+    "url": "/markreader/static/js/main.fd567f3c.chunk.js"
+  },
+  {
+    "revision": "f144951e1388e8b1fc48",
+    "url": "/markreader/static/js/runtime-main.8758bba7.js"
+  }
+]);
